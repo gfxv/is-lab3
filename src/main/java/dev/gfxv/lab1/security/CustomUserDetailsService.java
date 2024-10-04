707dce3798bcaaeb780479d7f533244bccb5d2a4
gfxv <superfreezu@gmail.com>
2024-10-04 17:19:08 +0300

    feat: add security stuff

---
+++ src/main/java/dev/gfxv/lab1/security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package dev.gfxv.lab1.security;
+
+import dev.gfxv.lab1.dao.RoleDAO;
+import dev.gfxv.lab1.dao.UserDAO;
+import dev.gfxv.lab1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDAO user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return new User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<GrantedAuthority> mapRolesToAuthorities(List<RoleDAO> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/dev/gfxv/lab1/security/SHA512Encoder.java
@@ -0,0 +1,36 @@
+package dev.gfxv.lab1.security;
+
+
+import org.springframework.security.crypto.codec.Utf8;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import org.apache.commons.codec.binary.Hex;
+
+public class SHA512Encoder implements PasswordEncoder {
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] hashedPassword = md.digest(rawPassword.toString().getBytes(StandardCharsets.UTF_8));
+            return Hex.encodeHexString(hashedPassword);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+
+        if (rawPassword == null || encodedPassword == null) {
+            return false;
+        }
+        return MessageDigest.isEqual(
+                Utf8.encode(encode(rawPassword)),
+                Utf8.encode(encodedPassword)
+        );
+    }
+}
