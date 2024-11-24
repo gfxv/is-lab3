0a615b1e969721d4a9eb94ba3321c517de778e93
gfxv <superfreezu@gmail.com>
2024-11-24 18:41:21 +0300

    feat: add Chapters By User endpoint

--- src/main/java/dev/gfxv/lab1/controller/ChapterController.java
+++ src/main/java/dev/gfxv/lab1/controller/ChapterController.java
@@ -1,15 +1,19 @@
 package dev.gfxv.lab1.controller;
 
-import dev.gfxv.lab1.repository.ChapterRepository;
-import dev.gfxv.lab1.repository.CoordinateRepository;
+import dev.gfxv.lab1.JwtParser;
+import dev.gfxv.lab1.dto.ChapterDTO;
+import dev.gfxv.lab1.security.JwtProvider;
+import dev.gfxv.lab1.service.ChapterService;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// TODO: add token checks
 
 @CrossOrigin
 @RestController
@@ -17,17 +21,31 @@ import org.springframework.web.bind.annotation.RestController;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ChapterController {
 
-    ChapterRepository chapterRepository;
+
+    ChapterService chapterService;
+    JwtProvider jwtProvider;
 
     @Autowired
     public ChapterController(
-        ChapterRepository chapterRepository
+            ChapterService chapterService,
+            JwtProvider jwtProvide
     ) {
-        this.chapterRepository = chapterRepository;
+        this.chapterService = chapterService;
+        this.jwtProvider = jwtProvide;
     }
 
     @GetMapping
     public ResponseEntity<?> getAllChapters() {
-        return ResponseEntity.ok(chapterRepository.findAll());
+        return ResponseEntity.ok(chapterService.getAllChapters());
+    }
+
+    @GetMapping("/my")
+    public ResponseEntity<?> getChaptersByUserId(
+        @RequestHeader("Authorization") String tokenHeader
+    ) {
+        String token = JwtParser.parseTokenFromHeader(tokenHeader);
+        String username = jwtProvider.getUsernameFromJwt(token);
+        List<ChapterDTO> chapters = chapterService.getChaptersByUser(username);
+        return new ResponseEntity<>(chapters, HttpStatus.OK);
     }
 }
--- src/main/java/dev/gfxv/lab1/repository/SpaceMarineRepository.java
+++ src/main/java/dev/gfxv/lab1/repository/SpaceMarineRepository.java
@@ -1,7 +1,11 @@
 package dev.gfxv.lab1.repository;
 
+import dev.gfxv.lab1.dao.ChapterDAO;
 import dev.gfxv.lab1.dao.SpaceMarineDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SpaceMarineRepository extends JpaRepository<SpaceMarineDAO, Long> {
+    List<SpaceMarineDAO> findAllByUserUsername(String name);
 }
