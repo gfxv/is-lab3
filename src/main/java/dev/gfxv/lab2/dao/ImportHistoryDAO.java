09b815e998caa80e459fba79d16555bad652799d
gfxv <superfreezu@gmail.com>
2024-12-14 19:54:29 +0300

    feat: add import functionality
    allow users to import space marine data via yml file.
    added import controller, service, repository and dao.
    implemented import history tracking.

---
+++ src/main/java/dev/gfxv/lab2/dao/ImportHistoryDAO.java
@@ -0,0 +1,35 @@
+package dev.gfxv.lab2.dao;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Entity
+@Table(name = "import_history")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ImportHistoryDAO {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "import-history-sequence-generator"
+    )
+    @SequenceGenerator(
+            name = "import-history-sequence-generator",
+            sequenceName = "import_history_id_seq",
+            allocationSize=1
+    )
+    Long id;
+
+    @Column(name="rows_added", nullable = false)
+    Integer rowsAdded;
+
+    @Column(nullable = false)
+    String status;
+
+    @ManyToOne
+    @JoinColumn(name="imported_by", referencedColumnName = "id")
+    UserDAO user;
+}
---
+++ src/main/java/dev/gfxv/lab2/exceptions/InvalidArgumentException.java
@@ -0,0 +1,8 @@
+package dev.gfxv.lab2.exceptions;
+
+public class InvalidArgumentException extends Exception{
+
+    public InvalidArgumentException(String message) {
+        super(message);
+    }
+}
